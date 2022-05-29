6c4e4c03bf4c7174ab81d8e2603c3dd17b3746ad
Brendon Maongera <bmaongera@gmail.com>
2022-05-29 23:06:27 +0200

    added payment id

--- context/payments/paymentProvider.js
+++ context/payments/paymentProvider.js
@@ -5,9 +5,13 @@ export function PaymentProvider({ children }) {
     const [payerName, setPayerName] = useState('');
     const [recipientPaymentEmail, setRecipientPaymentEmail] = useState('');
     const [amount, setAmount] = useState(0);
+    const [paymentId,setPaymentId] = useState('');
     return (
       <paymentContext.Provider 
-      value={{payerName, setPayerName,recipientPaymentEmail, setRecipientPaymentEmail,amount, setAmount}}>{children}
+      value={{payerName, setPayerName,
+      recipientPaymentEmail, setRecipientPaymentEmail,
+      amount, setAmount, 
+      paymentId, setPaymentId}}>{children}
         </paymentContext.Provider>
     );
   }
